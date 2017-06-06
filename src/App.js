be6f57bd581c4b6714bc8a59b7d894cbb1b8e728
wanghuan <196912334@qq.com>
2017-06-06 20:43:48 +0800

    新增todo

--- src/App.js
+++ src/App.js
@@ -9,10 +9,8 @@ class App extends Component {
   constructor(props){
     super(props)
     this.state  = {
-      newTodo: "test",
+      newTodo: "",
       todoList: [
-        {id:1,title:"第一个待办"},
-        {id:2,title:"第二个待办"},
       ]
     }
   }
@@ -29,14 +27,40 @@ class App extends Component {
       <div className="App">
         <h1>我的待办</h1>
         <div className="inputWrapper">
-          <TodoInput content={this.state.newTodo} />
+          <TodoInput content={this.state.newTodo}
+                    onChange={this.changeTitle.bind(this)} 
+                    onSubmit={this.addTodo.bind(this)} />
         </div>
         <ol>
           {todos}
         </ol>
       </div>
-    );
+    )
+  }
+  changeTitle(event){
+    this.setState({
+      newTodo: event.target.value,
+      todoList: this.state.todoList
+    })
+  }
+  addTodo(event){
+    this.state.todoList.push({
+      id:idMaker(),
+      title:event.target.value,
+      status:null,
+      deleted:false
+    })
+    this.setState({
+      newTodo:'',
+      todoList:this.state.todoList
+    })
   }
 }
 
 export default App;
+
+let id = 0
+function idMaker(){
+  id += 1
+  return id
+}
