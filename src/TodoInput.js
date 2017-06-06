c447b1d0604dd2f9edab26cd4e4f2a79ac8a6f65
wanghuan <196912334@qq.com>
2017-06-06 19:54:36 +0800

    将待办封装成TodoItem

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -1,6 +1,7 @@
-import React,{ Component } from 'react';
+import React, { Component } from 'react';
 
 export default class TodoInput extends Component{
     render(){
-        return <input type="text" value={this.state.newTodo}/>
+        return <input type="text" value={this.state.newTodo} />
+    }
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-// // ReactDOM.render(
-// //   <h1>Hello, world!</h1>,
-// //   document.getElementById('root2')
-// // );
-
-
-import Welcome from './Welcome'
+import App from './App'
 
 ReactDOM.render(
-  <Welcome name="robin"/>,
+  <App />,
   document.getElementById('root2')
 )
 
