5de00f6ab049a4253efe4759bb8f3f5a7a99ab8b
wanghuan <196912334@qq.com>
2017-06-07 21:42:59 +0800

    拷贝 leancloud 初始化代码

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ import 'normalize.css'
 import './reset.css'
 import * as localStore from './localStore'
 
+import AV from 'leancloud-storage'
+var APP_ID = 'GARswMsrzyUMGljYCRzKBV5E-gzGzoHsz';
+var APP_KEY = 'NVF25qMIJHly2xYS4qnOxeJe';
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+})
+
 class App extends Component {
   constructor(props){
     super(props)
